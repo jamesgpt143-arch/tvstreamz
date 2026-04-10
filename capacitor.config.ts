7daeb24d486e7c382e8b9523c861be14659c8fcf
jamesgpt143-arch <jamesgpt143@gmail.com>
2026-04-10 09:11:49 +0800

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,21 +1,21 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.tvstreamz.app',
+  appId: 'com.tvstreamz.app', // (Kung ano man ang appId mo)
   appName: 'TVStreamz',
   webDir: 'dist',
   bundledWebRuntime: false,
   plugins: {
     CapacitorHttp: {
-      // PATAYIN ITO: Ito ang sumisira sa Direct M3U8 Streams sa Android
+      // Panatilihin itong false para gumana ang Direct M3U8
       enabled: false, 
     },
   },
   server: {
     allowNavigation: [
       "*.supabase.co", 
-      "*.workers.dev",
-      "*"
+      "*.workers.dev"
+      // TININGGAL NATIN YUNG "*" DITO PARA LUMABAS NA ANG POPUPS SA SYSTEM BROWSER/SHOPEE APP
     ]
   }
 };
